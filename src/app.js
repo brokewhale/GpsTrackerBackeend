a51d65f6d4d7a2c65e1ab195a6640e18be658619
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2021-12-13 16:06:23 +0100

    dir removed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ let router = require("./routes/index");
 let bodyParser = require("body-parser");
 let cors = require("cors");
 
-global.__root = __dirname + "/";
+// global.__root = __dirname + "/";
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
